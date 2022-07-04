53bface40973dc1dc933f1bcc824f583b8b56b64
Alexander <raskalovalexander@gmail.com>
2022-07-04 12:43:14 +0300

    feat: mongodb was connected

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://raskalov:Hfcrfkjd98@cluster0.xkzc4.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('DB OK');
+  })
+  .catch((err) => {
+    console.log('DB ERROR', err);
+  });
 
 const app = express();
 
