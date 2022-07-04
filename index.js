3f988bbd1804a7edd1b587de899a100bfd79e898
Alexander <raskalovalexander@gmail.com>
2022-07-04 13:18:58 +0300

    feat: basic registration has been implemented

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
 
 import { registerValidator } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 mongoose
   .connect(
-    'mongodb+srv://raskalov:Hfcrfkjd98@cluster0.xkzc4.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://raskalov:Hfcrfkjd98@cluster0.xkzc4.mongodb.net/blog?retryWrites=true&w=majority'
   )
   .then(() => {
     console.log('DB OK');
@@ -20,14 +22,31 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/register', registerValidator, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+app.post('/register', registerValidator, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось зарегестрироваться, попробуйте еще',
+    });
   }
-  res.json({
-    success: true,
-  });
 });
 
 app.listen(3000, (err) => {
