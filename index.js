a5779322c1fe7d9d943de78bc148ebcb0c47ce9f
Alexander <raskalovalexander@gmail.com>
2022-07-04 09:43:58 +0300

    feat: jwt was added

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+    },
+    'dev-secret'
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
+});
 
 app.listen(3000, (err) => {
   if (err) {
