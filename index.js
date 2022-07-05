7fa9e2f381fe534cc4c818386e32032c0454322f
Alexander <raskalovalexander@gmail.com>
2022-07-05 16:04:15 +0300

    feat: cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import {
   loginValidator,
@@ -36,6 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post(
